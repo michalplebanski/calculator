7ade1b55a71af9d33ad56550c81864e6073222d6
Michał Plebański <michalplebanski@gmail.com>
2023-05-29 23:31:48 +0200

    Add operations

--- Desktop/Kodilla/Modul-4/calculator/main.py
+++ Desktop/Kodilla/Modul-4/calculator/main.py
@@ -1,28 +1,21 @@
-import logging
+from multiprocessing.sharedctypes import Value
 
-logging.basicConfig(level=logging.DEBUG)
 
-def calculator(action, number1, number2):
-    if action == 1:
-        logging.debug(f"Dodaję: {number1}, {number2}")
-        return number1 + number2
-    elif action == 2:
-        logging.debug(f"Odejmuję: {number1}, {number2}")
-        return number1 - number2
-    elif action == 3:
-        logging.debug(f"Mnozę: {number1}, {number2}")
-        return number1 * number2
-    elif action == 4:
-        logging.debug(f"Dzielę: {number1}, {number2}")
-        return number1 / number2
+def add(a, b):
+    return a + b
 
-if __name__ == '__main__':
-    logging.debug("Podaj działania, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-    action = int(input())
-    if action not in [1, 2, 3, 4, 5, 6]:
-        logging.debug("Podano złe działanie!")
-        exit(1)
-    number1 = int(input("Podaj pierwszą liczbę: "))
-    number2 = int(input("Podaj drugą liczbę: "))
-    result = calculator(action, number1, number2)
-    print(result)
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div 
+}
